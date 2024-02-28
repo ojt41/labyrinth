fc512a19a471022811f9c65c91165bed5bbb5bce
ojaswi <ojaswi.tyagi@aalto.fi>
2024-02-28 23:09:15 +0200

    added stub methods to storage class

--- src/main/scala/Storage.scala
+++ src/main/scala/Storage.scala
@@ -1,3 +1,7 @@
 class Storage {
+  def readMazeData(): Unit = ??? 
+  def writeMazeData(): Unit = ??? 
+  def saveHighscore(maze: Maze, score: Int): Unit = ??? 
+  def loadHighscore(maze: Maze): Int = ??? 
 
 }
