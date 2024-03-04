3d35707581257e7a94a9c44f0aca9d03005edcfa
ojaswi <ojaswi.tyagi@aalto.fi>
2024-03-04 14:15:36 +0200

    added moving methods

--- src/main/scala/Rat.scala
+++ src/main/scala/Rat.scala
@@ -1,4 +1,17 @@
+case class Rat(var currentPos: Passage) {
+  def moveUp(): Unit = {
+    currentPos = Passage(currentPos.row - 1, currentPos.col)
+  }
 
-class Rat(var currentPos: Passage){
+  def moveDown(): Unit = {
+    currentPos = Passage(currentPos.row + 1, currentPos.col)
+  }
 
+  def moveLeft(): Unit = {
+    currentPos = Passage(currentPos.row, currentPos.col - 1)
+  }
+
+  def moveRight(): Unit = {
+    currentPos = Passage(currentPos.row, currentPos.col + 1)
+  }
 }
