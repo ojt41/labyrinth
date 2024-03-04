a9da167f70744b2d516afd823e22d87793807ebb
ojaswi <ojaswi.tyagi@aalto.fi>
2024-03-04 14:23:14 +0200

    Rat movement refined by restricting within the passages

--- src/main/scala/Rat.scala
+++ src/main/scala/Rat.scala
@@ -1,17 +1,21 @@
-case class Rat(var currentPos: Passage) {
-  def moveUp(): Unit = {
-    currentPos = Passage(currentPos.row - 1, currentPos.col)
+class Rat(var currentPos: Passage) {
+  def moveUp(passages: Seq[Passage]): Unit = {
+    val newPos = Passage(currentPos.row - 1, currentPos.col)
+    if (passages.contains(newPos)) currentPos = newPos
   }
 
-  def moveDown(): Unit = {
-    currentPos = Passage(currentPos.row + 1, currentPos.col)
+  def moveDown(passages: Seq[Passage]): Unit = {
+    val newPos = Passage(currentPos.row + 1, currentPos.col)
+    if (passages.contains(newPos)) currentPos = newPos
   }
 
-  def moveLeft(): Unit = {
-    currentPos = Passage(currentPos.row, currentPos.col - 1)
+  def moveLeft(passages: Seq[Passage]): Unit = {
+    val newPos = Passage(currentPos.row, currentPos.col - 1)
+    if (passages.contains(newPos)) currentPos = newPos
   }
 
-  def moveRight(): Unit = {
-    currentPos = Passage(currentPos.row, currentPos.col + 1)
+  def moveRight(passages: Seq[Passage]): Unit = {
+    val newPos = Passage(currentPos.row, currentPos.col + 1)
+    if (passages.contains(newPos)) currentPos = newPos
   }
 }
