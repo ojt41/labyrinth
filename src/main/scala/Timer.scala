682719c0de55910715340a7d11b6984984128e69
ojaswi <ojaswi.tyagi@aalto.fi>
2024-03-04 14:23:39 +0200

    Timer class implemented

--- src/main/scala/Timer.scala
+++ src/main/scala/Timer.scala
@@ -1,5 +1,19 @@
-class Timer {
-  def startTime(): Unit = ???
-  def stopTime(): Unit = ???
-  def currentTime(): Int = ???
+case class Timer() {
+  var time: Int = 0
+
+  def update(): Unit = {
+    time += 1
+  }
+
+  def reset(): Unit = {
+    time = 0
+  }
+
+  def start(): Unit = {
+    time = 0
+  }
+
+  def stop(): Unit = {
+    // Stop timer logic here
+  }
 }
