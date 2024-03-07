c8851cc805ca597eafb1fc82e59e4f557cb79a86
ojaswi <ojaswi.tyagi@aalto.fi>
2024-03-07 16:48:54 +0200

    Introduced Moves taken instead of timer. Changed the Victory message.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -23,7 +23,8 @@ object MazeGUI extends JFXApp3 {
   var highlightSolution: Boolean = false
   var solution: Array[Passage] = Array.empty
 
-
+  var helpUsed = false
+  var movesTaken = 0
 
   def solveAndHighlight(): Unit = {
     solution = maze.solveMaze(rat)
@@ -33,11 +34,9 @@ object MazeGUI extends JFXApp3 {
     val alert = new Alert(AlertType.Information)
     alert.title = "Congratulations!"
     alert.headerText = "Victory!"
-    alert.contentText = s"You have reached the exit in seconds. Congratulations on solving the maze!"
+    alert.contentText = if (!helpUsed) then (s"You have reached the exit in ${movesTaken} moves. Congratulations on solving the maze!") else ("You didn't solve the maze on your own.")
     alert.showAndWait()
   }
-
-
 
   override def start(): Unit = {
     val dialogLength = new TextInputDialog() {
@@ -69,8 +68,9 @@ object MazeGUI extends JFXApp3 {
     rat = game.rat
     game.startGame(maze)
 
-    val canvasWidth = maze.wid * 20
-    val canvasHeight = maze.len * 20
+    val scaleFactor = 800 / ((mazeWid + length)/2) // Calculate scaling factor based on maze dimensions
+    val canvasWidth = scaleFactor * mazeWid
+    val canvasHeight = scaleFactor * length
 
     val canvas = new Canvas(canvasWidth, canvasHeight)
     val gc: GraphicsContext = canvas.graphicsContext2D
@@ -81,20 +81,20 @@ object MazeGUI extends JFXApp3 {
 
       gc.fill = Color.White
       maze.passages.foreach { passage =>
-        val x = passage.col * 20
-        val y = passage.row * 20
-        gc.fillRect(x, y, 20, 20)
+        val x = passage.col * scaleFactor
+        val y = passage.row * scaleFactor
+        gc.fillRect(x, y, scaleFactor, scaleFactor)
       }
 
       gc.fill = Color.Blue // Rat color
-      val ratX = rat.currentPos.col * 20
-      val ratY = rat.currentPos.row * 20
-      gc.fillRect(ratX, ratY, 20, 20)
+      val ratX = rat.currentPos.col * scaleFactor
+      val ratY = rat.currentPos.row * scaleFactor
+      gc.fillRect(ratX, ratY, scaleFactor, scaleFactor)
 
       gc.fill = Color.Green
-      val endX = maze.wid * 20 - 20
-      val endY = maze.len * 20 - 20
-      gc.fillRect(endX, endY, 20, 20)
+      val endX = mazeWid * scaleFactor - scaleFactor
+      val endY = length * scaleFactor - scaleFactor
+      gc.fillRect(endX, endY, scaleFactor, scaleFactor)
 
       gc.setStroke(Color.Red)
       gc.setLineWidth(3)
@@ -102,12 +102,12 @@ object MazeGUI extends JFXApp3 {
       maze.bridges.foreach { bridge =>
         val entrance1 = bridge.entrance1
         val entrance2 = bridge.entrance2
-        val x1 = entrance1.col * 20 + 10
-        val y1 = entrance1.row * 20 + 10
-        val x2 = entrance2.col * 20 + 10
-        val y2 = entrance2.row * 20 + 10
+        val x1 = entrance1.col * scaleFactor + scaleFactor / 2
+        val y1 = entrance1.row * scaleFactor + scaleFactor / 2
+        val x2 = entrance2.col * scaleFactor + scaleFactor / 2
+        val y2 = entrance2.row * scaleFactor + scaleFactor / 2
 
-        if (highlightSolution && (solution.contains(entrance1)) && (solution.contains(entrance2))) then {
+        if (highlightSolution && (solution.contains(entrance1)) && (solution.contains(entrance2))) {
           gc.setStroke(Color.Green)
         } else {
           gc.setStroke(Color.Red)
@@ -120,15 +120,15 @@ object MazeGUI extends JFXApp3 {
         gc.setStroke(Color.Green)
         gc.setLineWidth(2)
         solution.foreach { passage =>
-          val x = passage.col * 20 + 10
-          val y = passage.row * 20 + 10
-          gc.strokeOval(x, y, 5, 5)
+          val x = passage.col * scaleFactor + scaleFactor / 2
+          val y = passage.row * scaleFactor + scaleFactor / 2
+          gc.strokeOval(x, y, scaleFactor / 4, scaleFactor / 4)
         }
       }
 
       gc.setStroke(Color.DarkMagenta)
       gc.setLineWidth(1)
-      gc.strokeText(s"Time: seconds", 10, canvasHeight - 10)
+      gc.strokeText(s"Moves: ${movesTaken}", scaleFactor / 2, canvasHeight - scaleFactor / 2)
     }
 
     drawMaze()
@@ -140,23 +140,24 @@ object MazeGUI extends JFXApp3 {
       event.code match {
         case KeyCode.Up =>
           rat.moveUp(maze)
+          movesTaken += 1
         case KeyCode.Down =>
-
           rat.moveDown(maze)
+          movesTaken += 1
         case KeyCode.Left =>
-
           rat.moveLeft(maze)
+          movesTaken += 1
         case KeyCode.Right =>
-
           rat.moveRight(maze)
+          movesTaken += 1
         case KeyCode.Space =>
-
           rat.moveToOtherEnd(maze)
+          movesTaken += 1
         case KeyCode.H =>
           highlightSolution = true
+          helpUsed = true
           solveAndHighlight()
         case _ =>
-
       }
 
       if (rat.currentPos == Passage(maze.len - 1, maze.wid - 1)) {
@@ -182,8 +183,8 @@ object MazeGUI extends JFXApp3 {
 
     stage = new JFXApp3.PrimaryStage {
       title = "lost in maze"
-      width = canvasWidth + 20
-      height = canvasHeight + 100
+      width = canvasWidth + scaleFactor
+      height = canvasHeight + scaleFactor * 5
       scene = mainScene
     }
   }
