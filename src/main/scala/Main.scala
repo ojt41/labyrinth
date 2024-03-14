959ad13cf4ddc4fe04b3b06f467a57e28e5ed388
ojaswi <ojaswi.tyagi@aalto.fi>
2024-03-14 03:49:17 +0200

    Maze is always centered in the center now. Zooming works as expected (from the center)

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -206,29 +206,33 @@ object MazeGUI extends JFXApp3 {
     gc = canvas.graphicsContext2D
 
     def drawMaze(): Unit = {
+      // Calculate the centering offsets
+      val offsetX = (canvasWidth - mazeWid * scaleFactor) / 2
+      val offsetY = (canvasHeight - length * scaleFactor) / 2
+
       gc.fill = Color.Black
       gc.fillRect(0, 0, canvasWidth, canvasHeight)
 
       gc.fill = Color.White
       maze.passages.foreach { passage =>
-        val x = (passage.col + panOffsetX) * scaleFactor
-        val y = (passage.row + panOffsetY) * scaleFactor
+        val x = (passage.col + panOffsetX) * scaleFactor + offsetX
+        val y = (passage.row + panOffsetY) * scaleFactor + offsetY
         gc.fillRect(x, y, scaleFactor, scaleFactor)
       }
 
       gc.fill = Color.Blue // Rat color
-      val ratX = (rat.currentPos.col + panOffsetX) * scaleFactor
-      val ratY = (rat.currentPos.row + panOffsetY) * scaleFactor
+      val ratX = (rat.currentPos.col + panOffsetX) * scaleFactor + offsetX
+      val ratY = (rat.currentPos.row + panOffsetY) * scaleFactor + offsetY
       gc.fillRect(ratX, ratY, scaleFactor, scaleFactor)
 
       gc.fill = Color.Green
-      val endX = (mazeWid + panOffsetX) * scaleFactor - scaleFactor
-      val endY = (length + panOffsetY) * scaleFactor - scaleFactor
+      val endX = (mazeWid + panOffsetX) * scaleFactor - scaleFactor + offsetX
+      val endY = (length + panOffsetY) * scaleFactor - scaleFactor + offsetY
       gc.fillRect(endX, endY, scaleFactor, scaleFactor)
 
       gc.fill = Color.Red // enenmy
-      val opponentRatX = (opponentRat.currentPos.col + panOffsetX) * scaleFactor
-      val opponentRatY = (opponentRat.currentPos.row + panOffsetY) * scaleFactor
+      val opponentRatX = (opponentRat.currentPos.col + panOffsetX) * scaleFactor + offsetX
+      val opponentRatY = (opponentRat.currentPos.row + panOffsetY) * scaleFactor + offsetY
       gc.fillRect(opponentRatX, opponentRatY, scaleFactor, scaleFactor)
 
       val bridgeColor = Color.Blue
@@ -238,10 +242,10 @@ object MazeGUI extends JFXApp3 {
       maze.bridges.foreach { bridge =>
         val entrance1 = bridge.entrance1
         val entrance2 = bridge.entrance2
-        val x1 = (entrance1.col + panOffsetX) * scaleFactor + scaleFactor / 2
-        val y1 = (entrance1.row + panOffsetY) * scaleFactor + scaleFactor / 2
-        val x2 = (entrance2.col + panOffsetX) * scaleFactor + scaleFactor / 2
-        val y2 = (entrance2.row + panOffsetY) * scaleFactor + scaleFactor / 2
+        val x1 = (entrance1.col + panOffsetX) * scaleFactor + scaleFactor / 2 + offsetX
+        val y1 = (entrance1.row + panOffsetY) * scaleFactor + scaleFactor / 2 + offsetY
+        val x2 = (entrance2.col + panOffsetX) * scaleFactor + scaleFactor / 2 + offsetX
+        val y2 = (entrance2.row + panOffsetY) * scaleFactor + scaleFactor / 2 + offsetY
 
         if (highlightSolution && (solution.contains(entrance1)) && (solution.contains(entrance2))) {
           gc.setStroke(Color.Green)
@@ -256,6 +260,20 @@ object MazeGUI extends JFXApp3 {
         gc.setStroke(Color.Green)
         gc.setLineWidth(2)
         solution.foreach { passage =>
+          val x = (passage.col + panOffsetX) * scaleFactor + scaleFactor / 2 + offsetX
+          val y = (passage.row + panOffsetY) * scaleFactor + scaleFactor / 2 + offsetY
+          gc.strokeOval(x, y, scaleFactor / 4, scaleFactor / 4)
+        }
+      }
+
+      gc.setStroke(Color.OrangeRed)
+      gc.setLineWidth(1)
+      gc.strokeText(s"Moves: ${movesTaken}", scaleFactor / 2, canvasHeight - scaleFactor / 2)
+    
+      if (highlightSolution) {
+        gc.setStroke(Color.Green)
+        gc.setLineWidth(2)
+        solution.foreach { passage =>
           val x = (passage.col + panOffsetX) * scaleFactor + scaleFactor / 2
           val y = (passage.row + panOffsetY) * scaleFactor + scaleFactor / 2
           gc.strokeOval(x, y, scaleFactor / 4, scaleFactor / 4)
@@ -266,6 +284,7 @@ object MazeGUI extends JFXApp3 {
       gc.setLineWidth(1)
       gc.strokeText(s"Moves: ${movesTaken}", scaleFactor / 2, canvasHeight - scaleFactor / 2)
     }
+
 
 
 
