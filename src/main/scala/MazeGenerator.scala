3be34aa05a85f61cc9adc9452c6a670e0138b226
ojaswi <ojaswi.tyagi@aalto.fi>
2024-03-21 22:56:56 +0200

    Removed redundant method for bridges in MazeGenerator

--- src/main/scala/MazeGenerator.scala
+++ src/main/scala/MazeGenerator.scala
@@ -22,7 +22,7 @@ object MazeGenerator {
         grid(n.row)(n.col))
     }
 
-    // recursive function to generate maze
+    // recursive function to generate maze. Explained in detail in the document.
     def generate(givenCell: Cell): Unit = {
       var current = givenCell
       markVisited(current)
@@ -60,51 +60,6 @@ object MazeGenerator {
     grid
   }
 
-  // function to generate random bridges between passages
-  def generateBridges(rows: Int, cols: Int): Array[Bridge] = {
-    val bridges = mutable.Buffer[Bridge]() // stores generated bridges
-    val passages = for {
-      row <- 0 until rows
-      col <- 0 until cols
-    } yield Passage(row, col)
-
-    val randomPassages = Random.shuffle(passages)
-
-    var i = 0
-    while (i < randomPassages.length - 3) {
-      val passage1 = randomPassages(i)
-      val passage2 = randomPassages(i + 1)
-      val passage3 = randomPassages(i + 2)
-      val passage4 = randomPassages(i + 3)
-
-      // check if the passages are not in the same row or column to eliminate redundant bridges
-      if (passage1.row != passage2.row && passage1.col != passage2.col &&
-          passage3.row != passage4.row && passage3.col != passage4.col) {
-        val bridge1 = Bridge(passage1, passage2)
-        val bridge2 = Bridge(passage3, passage4)
-
-        // checking if bridges are overlapping
-        val overlaps = bridges.exists(existingBridge =>
-          (existingBridge.entrance1 == bridge1.entrance1 && existingBridge.entrance2 == bridge1.entrance2) ||
-          (existingBridge.entrance1 == bridge2.entrance1 && existingBridge.entrance2 == bridge2.entrance2) ||
-          (existingBridge.entrance1 == bridge1.entrance2 && existingBridge.entrance2 == bridge1.entrance1) ||
-          (existingBridge.entrance1 == bridge2.entrance2 && existingBridge.entrance2 == bridge2.entrance1)
-        )
-        
-        // if bridges don't overlap, add them to buffer.
-        if (!overlaps) {
-          bridges.append(bridge1)
-          bridges.append(bridge2)
-          i += 4
-        } else {
-          i += 1
-        }
-      } else {
-        i += 1
-      }
-    }
-    bridges.toArray
-  }
 
   // prints the maze with start and end points. Uncomment line 44 in Game.scala to use this function while playing the game.
   def printMazeWithStartAndEnd(grid: Array[Array[Boolean]], start: Cell, end: Cell): Unit = {
