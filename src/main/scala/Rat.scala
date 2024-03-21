5e88c4ab9ff7134078c9cfc000099639274e58d8
ojaswi <ojaswi.tyagi@aalto.fi>
2024-03-21 23:22:10 +0200

    Refactored Passage.scala, Rat.scala, and Wall.scala

--- src/main/scala/Rat.scala
+++ src/main/scala/Rat.scala
@@ -1,4 +1,6 @@
+// represents Rat which can move in the maze.
 class Rat(var currentPos: Passage) {
+  // basic movement (up,down,left,right)
   def moveUp(maze: Maze): Unit = {
     val newPos = Passage(currentPos.row - 1, currentPos.col)
     if (maze.hasPassage(newPos) || maze.hasBridge(currentPos, newPos)) currentPos = newPos
@@ -19,6 +21,7 @@ class Rat(var currentPos: Passage) {
     if (maze.hasPassage(newPos)|| maze.hasBridge(currentPos, newPos)) currentPos = newPos
   }
 
+  // move to the other end of the bridge
   def moveToOtherEnd(maze: Maze): Unit = {
     maze.bridges.find(bridge => bridge.entrance1 == currentPos || bridge.entrance2 == currentPos) match {
       case Some(bridge) => currentPos = if (currentPos == bridge.entrance1) bridge.entrance2 else bridge.entrance1
